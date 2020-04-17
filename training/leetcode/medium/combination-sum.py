1426ed24c3d7757ae3e54fe2e4354684fca79f2e
Thibaut <thibaut.passilly@epita.fr>
2020-04-17 09:47:45 +0200

    leet code : combination sum

---
+++ training/leetcode/medium/combination-sum.py
@@ -0,0 +1,18 @@
+class Solution:
+    def combinationSum(self, candidates, target):
+        candidates.sort(reverse=True)
+        results = []
+        for index, candidate in enumerate(candidates):
+            c = [candidate]
+            if sum(c) == target:
+                results.append(c)
+            elif sum(c) > target:
+                continue
+            for sub_candidates in self.combinationSum(candidates[index:], target - sum(c)):
+                results.append(c + sub_candidates)
+        return results
+
+s = Solution()
+print(s.combinationSum([2, 3, 6, 7], 7))
+print(s.combinationSum([2, 3, 5], 8))
+                
